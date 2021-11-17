526b8f243e24bee3b1a2f63dbb90fdd655840571
Cataline <catalinebaert@yahoo.com>
2021-11-17 14:41:29 +0100

    Implement 'add a review to a restaurant' feature and display them in the restaurant show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
 
-  resources :restaurants, only: [:new, :create, :show]
+  resources :restaurants, only: [:new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
