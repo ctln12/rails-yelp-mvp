eca0c0b622fd1d45e1a0d4c92b89a2c139076c3c
Cataline <catalinebaert@yahoo.com>
2021-11-17 14:20:02 +0100

    Implement 'see the details of a restaurant' feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,6 +16,10 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
 
   def restaurant_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
 
-  resources :restaurants, only: [:new, :create]
+  resources :restaurants, only: [:new, :create, :show]
 end
