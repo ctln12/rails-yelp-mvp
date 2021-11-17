a98ff3a584e2f4ec8466ade61176064c9e63f18a
Cataline <catalinebaert@yahoo.com>
2021-11-17 14:53:33 +0100

    Add index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
 
-  resources :restaurants, only: [:new, :create, :show] do
+  resources :restaurants, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :create]
   end
 end
