7b80a1006158e3d404e1370337f7f9fcb000b712
Cataline <catalinebaert@yahoo.com>
2021-11-17 13:33:26 +0100

    Seed 5 restaurants using the faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all if Rails.env.development?
+
+cities = ['Lausanne', 'Renens', 'Morges', 'Nyon']
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Creating 5 restaurants...'
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: cities.sample,
+    phone_number: "021#{rand.to_s[2..8]}",
+    category: categories.sample
+  )
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished'
